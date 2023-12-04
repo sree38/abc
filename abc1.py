df691145a3885229ff61b7444daf3db31ed0fa03
sreebala <110999114+sree38@users.noreply.github.com>
2023-12-04 12:50:34 +0530

    Add files via upload

---
+++ abc1.py
@@ -0,0 +1,10 @@
+"""name=input("enter your name");
+dob=input("enter dob");
+class1=input("enter class");
+l=[1,'abc',900]
+print("name");
+print("dob");
+print(class1);
+print(l);"""
+
+print("Welcome to Python");
---
+++ abc2.py
@@ -0,0 +1,4 @@
+l=[1,2,3,'g',4,5,6,9]
+print(l);
+print("This is my new program");
+print("welcome to python");
